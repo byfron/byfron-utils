ba70924e92b28713ea206baf5749b1f116240e8f
byfron <josecrubio82@gmail.com>
2017-02-05 12:47:07 +0100

    Adds templated object pool

---
+++ src/Pool.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <memory>
+#include <queue>
+
+template <typename T>
+class Pool : public std::enable_shared_from_this< Pool<T> >{
+
+private:
+	struct ExternalDeleter {
+		explicit ExternalDeleter(std::weak_ptr<Pool<T> > pool)
+			: m_pool(pool) {}
+
+		void operator()(T* ptr) {
+			if (auto pool_ptr = m_pool.lock()) {
+				try {
+					(pool_ptr->add(std::unique_ptr<T>(ptr)));
+					return;
+				} catch(...) {}
+			}
+			std::default_delete<T>{}(ptr);
+		}
+	private:
+		std::weak_ptr<Pool<T> > m_pool;
+	};
+
+	std::queue<std::unique_ptr<T> > m_pool;
+
+public:
+
+	using PtrType = std::unique_ptr<T, ExternalDeleter>;	
+
+	Pool() {}
+
+	void add(std::unique_ptr<T> t) {
+		m_pool.push(std::move(t));
+	}
+	
+	PtrType acquire() {
+		assert(!m_pool.empty());
+		PtrType tmp(m_pool.front().release(), ExternalDeleter(this->shared_from_this()));
+		m_pool.pop();
+		return std::move(tmp);
+	}
+
+	bool empty() const {
+		return m_pool.empty();
+	}
+
+	size_t size() const {
+		return m_pool.size();
+	}
+	
+};
+
---
+++ test/test_Pool.cpp
@@ -0,0 +1,21 @@
+#include "gtest.h"
+#include "Pool.hpp"
+#include <unistd.h>
+
+TEST(TestPool, Pool) {
+
+	std::cout << "Pool" << std::endl;
+	std::shared_ptr<Pool<int> > pool = std::make_shared<Pool<int> >();
+	EXPECT_TRUE(pool->empty());
+
+	pool->add(std::unique_ptr<int>(new int(45)));
+	EXPECT_TRUE(not pool->empty());
+
+	{
+	auto v = pool->acquire();
+	EXPECT_TRUE(pool->empty());
+	EXPECT_TRUE(*v == 45);
+	}
+
+	EXPECT_TRUE(not pool->empty());	
+}
